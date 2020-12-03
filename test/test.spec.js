d02ab6d408d5c8cf1aaffe705eebc146163c3b6f
TheOverwatcher <graysonbonds@gmail.com>
2020-12-03 11:34:05 -0500

    Refactor to place common vars in scope of tests;

--- test/test.spec.js
+++ test/test.spec.js
@@ -2,12 +2,22 @@ const KeyGenerator = require('../src/index.js')
 const assert = require('assert')
 
 describe('Sodium Encryption Tool provides a public/private key pair', function(){
-    let keys, keyGen;
+    let encryptedMessage, decryptedMessage, keys, keyGen;
     before(async function(){
         keyGen = new KeyGenerator({
             'message': 'Hello World'
         });
         keys = await keyGen.generateKeys();
+
+        encryptedMessage = await keyGen.encrypt({
+            'publicKey':keys.publicKey,
+        });
+
+        decryptedMessage = await keyGen.decrypt({
+            'encryptedMessage':encryptedMessage,
+            'publicKey':keys.publicKey,
+            'privateKey':keys.privateKey
+        });
     });
     it('Sodium is ready to generate a pair of keys', function(){
         assert(keys, 'Keys not provided. Sodium has not returned them.')
@@ -19,20 +29,9 @@ describe('Sodium Encryption Tool provides a public/private key pair', function()
         assert(keys.privateKey, 'Invalid private key provided');
     });
     it('Public Key can encrypt a message', async function(){
-        let encryptedMessage = await keyGen.encrypt({
-            'publicKey':keys.publicKey,
-        });
         assert(encryptedMessage.toString().length > 0, 'Invalid encrypted message provided');
     });
     it('Public and Private Keys can decrypt the encrypted message', async function(){
-        let encryptedMessage = await keyGen.encrypt({
-            'publicKey':keys.publicKey,
-        });
-        let decryptedMessage = await keyGen.decrypt({
-            'encryptedMessage':encryptedMessage,
-            'publicKey':keys.publicKey,
-            'privateKey':keys.privateKey
-        });
         assert(decryptedMessage && decryptedMessage.toString().length > 0, 'Invalid encrypted message provided');
     });
 })
