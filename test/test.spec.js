d0606a651b9c3767b5745f9c7be0300a613dd502
TheOverwatcher <graysonbonds@gmail.com>
2020-12-03 12:08:11 -0500

    Update test messages to be more direct

--- test/test.spec.js
+++ test/test.spec.js
@@ -19,10 +19,10 @@ describe('Sodium Encryption Tool provides a public/private key pair', function()
     it('Sodium is ready to generate a pair of keys', function(){
         assert(keyGen.keys, 'Keys not provided. Sodium has not returned them.')
     });
-    it('Public Key provided', function() {
+    it('Public Key was generated', function() {
         assert(keys.publicKey, 'Invalid public key provided');
     });
-    it('Private Key provided', function() {
+    it('Private Key was generated', function() {
         assert(keys.privateKey, 'Invalid private key provided');
     });
     it('Public Key can encrypt a message', async function(){
@@ -31,7 +31,7 @@ describe('Sodium Encryption Tool provides a public/private key pair', function()
     it('Public and Private Keys can decrypt the encrypted message', async function(){
         assert(decryptedMessage && decryptedMessage.toString().length > 0, 'Invalid encrypted message provided');
     });
-    it('Verify decrypted message matches the original message', function() {
+    it('The decrypted message matches the original message', function() {
         assert(keyGen.doesMessageMatch({
             'decryptedMessage':decryptedMessage
         }),'Messages do not match after decryption.');
