b486157103e36052a6473355c37dcade528c0988
TheOverwatcher <graysonbonds@gmail.com>
2020-12-04 15:08:41 -0500

    Refactor index to output to file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ const KeyGenerator = require('../src/keyGenerator.js')
 
 const keyGen = new KeyGenerator();
 
-if(async () => await keyGen.process() === true){
-    console.log('Successfully generated keys and an ecrypted message.')
-}
+keyGen.process();
